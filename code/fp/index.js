bcec4eaecb0a925747b2f0a6e4a769525c5a7bf2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-31 14:29:02 -0400

    small functions

--- code/fp/index.js
+++ code/fp/index.js
@@ -4,10 +4,18 @@
 // even numbers by a constant and print the result
 var numbers = [3, 2, 8]
 var constant = 2
-
 var k = 0
+function mul(a, b) {
+  return a * b
+}
+function print(x) {
+  console.log(x)
+}
+function isEven(x) {
+  return x % 2 === 0
+}
 for (k = 0; k < numbers.length; k += 1) {
-  if (numbers[k] % 2 === 0) {
-    console.log(numbers[k] * constant)
+  if (isEven(numbers[k])) {
+    print(mul(numbers[k], constant))
   }
 }
