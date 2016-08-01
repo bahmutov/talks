1564f53a3163f9c99f0f43a04a340655079ac19c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-31 23:11:00 -0400

    promises using pipeP

--- code/fp/using-ramda.js
+++ code/fp/using-ramda.js
@@ -1,6 +1,6 @@
 'use strict'
 const R = require('ramda')
-var numbers = [3, 2, 8]
+const numbers = () => Promise.resolve([3, 2, 8])
 var constant = 2
 function print(x) {
   console.log(x)
@@ -8,9 +8,10 @@ function print(x) {
 function isEven(x) {
   return x % 2 === 0
 }
-const solution = R.pipe(
+const solution = R.pipeP(
+  numbers,
   R.filter(isEven),
-  R.map(R.multiply(constant)),
-  R.forEach(print)
+  R.map(R.multiply(constant))
 )
-solution(numbers)
+solution()
+  .then(R.forEach(print))
