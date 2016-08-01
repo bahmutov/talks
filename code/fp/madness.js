6da393d60470e9fb588306002a72ca7e5284e1b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-31 22:44:14 -0400

    partial right

--- code/fp/madness.js
+++ code/fp/madness.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const unary = f => x => f(x)
+const partialRight = (f, b) => a => f(a, b)
 
 console.log(['1', '2', '3'].map(parseFloat))
 // 1, 2, 3
 console.log(['1', '2', '3'].map(parseInt))
 // 1, NaN, NaN
 console.log(['1', '2', '3'].map(unary(parseInt)))
+// 1, 2, 3
+const parse10 = partialRight(parseInt, 10)
+console.log(['1', '2', '3'].map(parse10))
 // 1, 2, 3
